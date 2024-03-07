000dd0816b8200542c1179a2d0fc63be5e3b9246
Milton Triviño Fuquen <miltonbass@gmail.com>
2024-03-07 11:04:31 -0500

    Begin repository patten

--- Orders/Orders/Orders.Frontend/Program.cs
+++ Orders/Orders/Orders.Frontend/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Orders.Frontend;
+using Orders.Frontend.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//conectar el backend
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5291//") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
